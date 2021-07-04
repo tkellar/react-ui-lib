d66ca4d6501d3dc1910ebbf4fcfcd6f22b8aa10e
tkellar <tristandkellar@gmail.com>
2021-07-04 11:21:05 -0400

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-ui-lib'
+import 'react-ui-lib/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
