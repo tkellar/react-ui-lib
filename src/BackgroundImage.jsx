cdc16a7e134ef961fef689e3c3a879ee4da09fce
tkellar <tristandkellar@gmail.com>
2021-07-05 21:17:35 -0400

    Remove dependency on styled-components

--- src/BackgroundImage.jsx
+++ src/BackgroundImage.jsx
@@ -1,28 +1,26 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const Div = styled.div`
-  background-image: ${({ imgSrc }) => `url(${imgSrc})`};
-  background-repeat: none;
-  background-size: cover;
-`
-const Overlay = styled.div`
-  height: 100%;
-  width: 100%;
-`
 
 const BackgroundImage = (props) => {
   const { children, imgSrc, style, fullScreen, overlayStyle } = props
 
-  let styles = style
+  const styles = {
+    backgroundImage: `url(${imgSrc})`,
+    backgroundRepeat: 'none',
+    backgroundSize: 'cover',
+    ...style
+  }
+
   if (fullScreen) {
-    styles = { ...style, height: '100vh', width: '100vw' }
+    styles.height = '100vh'
+    styles.width = '100vw'
   }
 
   return (
-    <Div className='BackgroundImage' style={styles} imgSrc={imgSrc}>
-      <Overlay style={overlayStyle}>{children}</Overlay>
-    </Div>
+    <div className='BackgroundImage' style={styles} imgSrc={imgSrc}>
+      <div style={{ height: '100%', width: '100%', ...overlayStyle }}>
+        {children}
+      </div>
+    </div>
   )
 }
 
